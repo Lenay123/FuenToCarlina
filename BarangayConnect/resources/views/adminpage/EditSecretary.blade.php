b978eaf6bf6e93cd52d26755ba788f30d0cf0146
Ma Elena FUENTES <ma.fuentes@datawords.com>
2023-10-24 18:45:25 +0800

    puyaaaaaaa

--- BarangayConnect/resources/views/adminpage/EditSecretary.blade.php
+++ BarangayConnect/resources/views/adminpage/EditSecretary.blade.php
@@ -8,7 +8,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
-
+  <link rel="icon" href="{{ asset('img/image (5).png') }}">
   <title>Barangay Connect</title>
 
   <!-- Custom fonts for this template -->
