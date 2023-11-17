ca36b42f0fe59e9004c7e19713268656e7b2350c
Dev-Opora <122242901+Dev-Opora@users.noreply.github.com>
2023-11-17 15:54:04 +0800

    migrate:rollback

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -4,7 +4,7 @@ composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
 echo "generating application key..."
-php artisan key:generate --show
+# php artisan key:generate --show
 
 echo "Caching config..."
 php artisan config:cache
@@ -14,3 +14,6 @@ php artisan route:cache
 
 echo "Running migrations..."
 php artisan migrate --force
+
+echo "Rollback"
+php artisan migrate:rollback
