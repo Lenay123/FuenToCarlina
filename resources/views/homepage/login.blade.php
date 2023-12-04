5ed5fef7dcb26f58e30efb71b7575e99c1267d23
Ma Elena FUENTES <ma.fuentes@datawords.com>
2023-12-04 13:06:49 +0800

    changed

--- resources/views/homepage/login.blade.php
+++ resources/views/homepage/login.blade.php
@@ -6,6 +6,7 @@
     <title>Barangay Connect</title>
     <link rel="icon" href="{{ asset('img/image (5).png') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 
 </head>
 <style>
@@ -37,84 +38,85 @@
     cursor: pointer;
 }
 
-
-
 </style>
 <link rel="stylesheet" href="/css/login.css">
 <body>
-  <div class="mt-5">
-    @if($errors->any())
-        <div class="col-12">
-            @foreach($errors->all() as $error)
-                <div class="alert alert-danger">{{$error}}</div>
-            @endforeach
-        </div>
-    @endif
-
-    @if(session()->has('error'))
-        <div class="alert alert-danger">{{ session('error') }}</div>
-    @endif
-
-    @if(session()->has('success'))
-        <div class="alert alert-success">{{ session('success') }}</div>
-    @endif
-</div>
-
-
-
-  <div class="vertical-line"></div>
+    <div class="vertical-line"></div>
     <section>
         <div class="container">
-
-         
-          <div class="user signinBx">
-            <div class="imgBx"><img src="{{ asset('img/image (5).png') }}" alt=""id="responsive-image"/></div>
-           
-            <div class="formBx">
-              <form action="{{ route('login.post') }}" method="POST">
-                @csrf
-                <h2>Sign In</h2>
-                
-                <div class="password-container">
-                  <input type="text" name="email" placeholder="Email" required/>
-                  <span class="toggle-password">
-                    <i class="far fa-envelope"  style="margin-left: -30px; cursor: pointer;"></i>
-                  </span>
-  
-              </div>
-                <div class="password-container">
-                  <input type="password" name="password" autocomplete="current-password" required="" id="id_password" placeholder="Enter Password"/>
-                  <span class="toggle-password">
-                  <i class="far fa-eye" id="togglePassword" style="margin-left: -30px; cursor: pointer;"></i>
-                  </span>
-
-              </div>
-
-                <input type="submit" name="" value="Login" />
-                <p class="signup">
-                    Don't have an account ?
-                    <a href="{{ route('register') }}">Sign Up.</a>
-                    or
-                    <a href="/">BACK.</a>
-                </p>
-            </form>
+            <div class="user signinBx">
+                <div class="imgBx"><img src="{{ asset('img/image (5).png') }}" alt="" id="responsive-image"/></div>
+                <div class="formBx">
+                    <form action="{{ route('login.post') }}" method="POST">
+                        @csrf
+                        <h2>Sign In</h2>
+                        <div class="password-container">
+                        <input type="text" name="email" placeholder="Email" value="{{ old('email') }}" required />
+                            <span class="toggle-password">
+                                <i class="far fa-envelope" style="margin-left: -30px; cursor: pointer;"></i>
+                            </span>
+                        </div>
+                        <div class="password-container">
+                            <input type="password" name="password" autocomplete="current-password" required="" id="id_password" placeholder="Enter Password"/>
+                            <span class="toggle-password">
+                                <i class="far fa-eye" id="togglePassword" style="margin-left: -30px; cursor: pointer;"></i>
+                            </span>
+                        </div>
+                        <input type="submit" name="" value="Login" />
+                        <p class="signup">
+                            Don't have an account ?
+                            <a href="{{ route('register') }}">Sign Up.</a>
+                            or
+                            <a href="/">BACK.</a>
+                        </p>
+                    </form>
+                </div>
             </div>
-          </div>
         </div>
-        
-      </section>
+    </section>
+    <!-- Modal -->
+    <div class="modal fade" id="errorModal" tabindex="-1" role="dialog" aria-labelledby="errorModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="errorModalLabel">Error</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    @if(session()->has('error'))
+                        {{ session('error') }}
+                    @endif
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Bootstrap and other scripts -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script>
+        const togglePassword = document.querySelector('#togglePassword');
+        const password = document.querySelector('#id_password');
 
-<script>
-    const togglePassword = document.querySelector('#togglePassword');
-      const password = document.querySelector('#id_password');
-
-      togglePassword.addEventListener('click', function (e) {
-        // toggle the type attribute
-        const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-        password.setAttribute('type', type);
-        // toggle the eye slash icon
-        this.classList.toggle('fa-eye-slash');
-    });
-</script>
-    </body>
+        togglePassword.addEventListener('click', function (e) {
+            // toggle the type attribute
+            const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+            password.setAttribute('type', type);
+            // toggle the eye slash icon
+            this.classList.toggle('fa-eye-slash');
+        });
+    </script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            @if(session()->has('error'))
+                $('#errorModal').modal('show');
+            @endif
+        });
+    </script>
+</body>
 </html>
